b13074b0b65d6a12b0e4caf84df9c79c24d5b28e
LabTS <wheelhousemark+labts@gmail.com>
2025-02-01 01:05:43 +0000

    provided code

---
+++ src/test/kotlin/simulation/SSQTest.kt
@@ -0,0 +1,19 @@
+package simulation
+
+// import org.junit.Test
+// import kotlin.test.assertEquals
+
+class SSQTest {
+
+    // @Test
+    // fun `matches mm1 queueing theory`() {
+    //     val lambda = 4.0
+    //     val mu = 5.0
+    //     val randomSeed = 12345L
+    //     val stoppingTime = 1000000.0
+    //     val expectedLongRunQueueLength = (lambda / mu) / (1 - lambda / mu) // = 4.0
+    //     // assertEquals(expectedLongRunQueueLength, MM1Queue(lambda, mu, stoppingTime).runSim(), 0.01)
+    //     // For the extension "Deterministic Random Numbers"
+    //     assertEquals(expectedLongRunQueueLength, MM1Queue(lambda, mu, randomSeed, stoppingTime).runSim(), 0.01)
+    // }
+}
